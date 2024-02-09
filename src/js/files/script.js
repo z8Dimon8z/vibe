4cbc545c86a2e69689e8821290cf137c173fb7e8
Dima Lysak <60256445+z8Dimon8z@users.noreply.github.com>
2024-02-09 05:16:10 +0300

    подключил видео

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -4,4 +4,14 @@ import { isMobile } from "./functions.js";
 import { flsModules } from "./modules.js";
 
 // Свой код пишем здесь
+// библиотка fansybox предварительно установить через nodemodules
+// документация https://fancyapps.com/fancybox/getting-started/
+
+import {
+  Fancybox
+} from "@fancyapps/ui";
+
+Fancybox.bind("[data-fancybox]", {
+  // Your custom options
+});
 
